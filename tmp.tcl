d78fa6e2604276619e0bc4bee12d31aa338f9297
Kent B. Larsen <kent.b.larsen@gmail.com>
2016-12-08 21:37:05 -0800

    added more walls/windows/doors

--- tmp.tcl
+++ tmp.tcl
@@ -1,11 +1,6 @@
 set default "index.html"
 
 
-#proc respond {sock code body {head "\n"}} {
-#  puts -nonewline $sock "HTTP/1.1 $code OK\nContent-Type: text/html; \
-#         \nConnection: keep-alive\nContent-length: [string length $body]\n$head\n$body"
-#}
-
 proc EchoAccept {sock addr port} {
   global echo
   set echo(addr,$sock) [list $addr $port]
@@ -13,7 +8,7 @@ proc EchoAccept {sock addr port} {
 }
 
 proc BuildStyle {} {
-    global scontent
+	global scontent
 	global wallColor
 	global winColor
 	global doorColor
@@ -67,15 +62,6 @@ proc BuildBody {sock} {
     append bcontent "<meta http-equiv=\"refresh\" content=\"5\">"
 
 	append bcontent $fixedBodyContent
-	
-#	buildFixedWindow "master_bath_window_11" $Top 100 20 0 3 
-#	buildFixedWindow "master_bath_window_21" $Top 192 30 0 3 
-#	buildFixedWindow "dining_window_12" $Top 336 60 0 3 
-#	buildFixedWindow "kitchen_window_11" $Top 514 40 0 3 
-#	buildFixedWindow "boy_window_11" $Top 840 40 0 3 
-#	buildFixedWindow "n_window_11" 320 982 0 40 3 
-#	buildFixedWindow "toy_window_11" 554 892 40 0 3 
- 
 
     append bcontent "</body>"
     append bcontent "</html>"
@@ -89,26 +75,71 @@ proc BuildBody {sock} {
     incr echo(counter)
 }
 
+#proc Echo {sock} {
+#  global echo
+#  fconfigure $sock -blocking 0
+#  set line [gets $sock]
+#  gets $sock lline
+#  while {[string compare $lline ""] != 0} {
+#    gets $sock lline
+#  }
+#  set shortName "/"
+#  regexp {/[^ ]*} $line shortName
+#  set many [string length $shortName]
+#  set last [string index $shortName [expr {$many-1}]]
+#  if {$last=="/"} then {set shortName "/"}
+#  set wholeName $shortName
+#    if {$wholeName=="/"} {
+#        BuildBody $sock
+#    } else {
+##        set imgFile [string range $wholeName 1 end]
+##        BuildPng $sock $imgFile
+#    }
+#}
+
 proc Echo {sock} {
   global echo
   fconfigure $sock -blocking 0
   set line [gets $sock]
+  gets $sock lline
+  while {[string compare $lline ""] != 0} {
     gets $sock lline
-    while {[string compare $lline ""] != 0} {
-        gets $sock lline
-    }
-  set shortName "/"
-  regexp {/[^ ]*} $line shortName
-  set many [string length $shortName]
-  set last [string index $shortName [expr {$many-1}]]
-  if {$last=="/"} then {set shortName "/"}
-  set wholeName $shortName
-    if {$wholeName=="/"} {
-        BuildBody $sock
-    } else {
-#        set imgFile [string range $wholeName 1 end]
-#        BuildPng $sock $imgFile
-    }
+  }
+  BuildBody $sock
+}
+
+proc BuildFixed {filename} {
+	global fixedBodyContent
+	global echo
+
+	set fp [open $echo(home)/fixed.txt r]
+	fconfigure $fp -buffering line
+	while {true} {
+		if [eof $fp] {
+			catch {close $fp}
+			break
+		}
+		gets $fp line
+		if {[string length $line] < 5} {
+			catch {close $fp}
+			break
+		}
+		#puts $line
+		set Tag [lindex $line 0]
+		if {$Tag eq "vwall"} {
+			set Length "height:"
+		} elseif {$Tag eq "vwin"} {
+			set Length "height:"
+		} elseif {$Tag eq "vdoor"} {
+			set Length "height:"
+		} else {
+			set Length "width:"
+		}
+		set Top [lindex $line 1]
+		set Left [lindex $line 2]
+		append Length [lindex $line 3]
+		append fixedBodyContent "<$Tag style=\"top:${Top}px;left:${Left}px;${Length}px\"></$Tag>\r\n"
+	}
 }
 
 set Left 50
@@ -123,38 +154,11 @@ set fixedBodyContent {}
 set echo(counter) 0
 set echo(port) [lindex $argv 0]
 set echo(home) [lindex $argv 1]
-set fp [open $echo(home)/fixed.txt r]
-fconfigure $fp -buffering line
-while {true} {
-	if [eof $fp] {
-		catch {close $fp}
-		break
-	}
-	gets $fp line
-	if {[string length $line] < 5} {
-		catch {close $fp}
-		break
-	}
-	puts $line
-	set Tag [lindex $line 0]
-	if {$Tag eq "vwall"} {
-		set Length "height:"
-	} elseif {$Tag eq "vwin"} {
-		set Length "height:"
-	} elseif {$Tag eq "vdoor"} {
-		set Length "height:"
-	} else {
-		set Length "width:"
-	}
-	set Top [lindex $line 1]
-	set Left [lindex $line 2]
-	append Length [lindex $line 3]
-	append fixedBodyContent "<$Tag style=\"top:${Top}px;left:${Left}px;${Length}px\"></$Tag>\r\n"
-}
-puts $fixedBodyContent
 BuildStyle
+BuildFixed $echo(home)/fixed.txt
+#puts $fixedBodyContent
 #puts $scontent
 puts "Socket Port $echo(port);  Home $echo(home)"
 set echo(main) [socket -server EchoAccept $echo(port)]
 vwait forever
-  
+ 
